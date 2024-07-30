cda6b0d08363d483ab95c68a7cbc3a033a6a221f
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2024-07-30 13:25:28 +0500

    (server) get all posts controller

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -24,7 +24,28 @@ const PostController = {
     }
   },
   getAllPosts: async (req, res) => {
-    res.send("get all");
+    const { userId } = req.user;
+
+    try {
+      const posts = await prisma.post.findMany({
+        include: {
+          likes: true,
+          author: true,
+          comments: true,
+        },
+        orderBy: { createdAt: "desc" },
+      });
+
+      const postWithLikeInfo = posts.map((post) => ({
+        ...post,
+        likedByUser: post.likes.some((like) => like.userId === userId),
+      }));
+
+      res.status(200).json(postWithLikeInfo);
+    } catch (error) {
+      console.error("Error in Get All posts", error);
+      res.status(500).json({ error: "Internal server error!" });
+    }
   },
   getPostById: async (req, res) => {
     res.send("get post by id");
