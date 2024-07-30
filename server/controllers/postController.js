29ca1f8055a901539b8d025e5f16b7b5094c6f91
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2024-07-30 15:32:05 +0500

    (server) delete post controller

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -85,7 +85,33 @@ const PostController = {
     }
   },
   deletePost: async (req, res) => {
-    res.send("delete post");
+    const { id } = req.params;
+
+    const isValidObjectId = /^[a-fA-F0-9]{24}$/.test(id);
+    if (!isValidObjectId) {
+      console.error("Invalid ObjectID format:", id);
+      return res.status(400).json({ error: "Invalid Id!" });
+    }
+
+    const post = await prisma.post.findUnique({ where: { id } });
+
+    if (!post) return res.status(404).json({ error: "Post not found!" });
+
+    if (post.authorId != req.user.userId)
+      return res.status(403).json({ error: "No access!" });
+
+    try {
+      const transaction = await prisma.$transaction([
+        prisma.comment.deleteMany({ where: { postId: id } }),
+        prisma.like.deleteMany({ where: { postId: id } }),
+        prisma.post.delete({ where: { id } }),
+      ]);
+
+      res.status(200).json(transaction);
+    } catch (error) {
+      console.error("Error in Delete Post", error);
+      res.status(500).json({ error: "Internal server error!" });
+    }
   },
 };
 
