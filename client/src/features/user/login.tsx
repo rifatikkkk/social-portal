738ca377df7b7cc37444a713c74a10356d3c396c
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2024-09-11 19:29:33 +0300

    (client) login and register

---
+++ client/src/features/user/login.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react"
+import { useForm } from "react-hook-form"
+import { Input } from "../../components/input"
+import { Button, Link } from "@nextui-org/react"
+import {
+  useLazyCurrentQuery,
+  useLoginMutation,
+} from "../../app/services/userApi"
+import { useNavigate } from "react-router-dom"
+import { ErrorMessage } from "../../components/error-message"
+import { hasErrorField } from "../../utils/has-error-field"
+
+type Login = {
+  email: string
+  password: string
+}
+
+type Props = {
+  setSelected: (value: string) => void
+}
+
+export const Login: React.FC<Props> = ({ setSelected }) => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<Login>({
+    mode: "onChange",
+    reValidateMode: "onBlur",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
+  const [login, { isLoading }] = useLoginMutation()
+  const navigate = useNavigate()
+  const [error, setError] = useState("")
+  const [triggerCurrentQuery] = useLazyCurrentQuery()
+
+  const onSubmit = async (data: Login) => {
+    try {
+      await login(data).unwrap()
+      await triggerCurrentQuery().unwrap()
+      navigate("/")
+    } catch (error) {
+      if (hasErrorField(error)) {
+        setError(error.data.error)
+      }
+    }
+  }
+
+  return (
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        control={control}
+        name="email"
+        label="Email"
+        // type="email"
+        required="Required fields"
+      />
+      <Input
+        control={control}
+        name="password"
+        label="Password"
+        type="password"
+        required="Required fields"
+      />
+
+      <ErrorMessage error={error} />
+
+      <p className="text-center text-small">
+        Don't have an account?{" "}
+        <Link
+          size="sm"
+          className="cursor-pointer"
+          onPress={() => setSelected("sign-up")}
+        >
+          Register
+        </Link>
+      </p>
+      <div className="flex gap-2 justify-end">
+        <Button fullWidth color="primary" type="submit" isLoading={isLoading}>
+          Login
+        </Button>
+      </div>
+    </form>
+  )
+}
---
+++ client/src/features/user/register.tsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react"
+import { Input } from "../../components/input"
+import { Button, Link } from "@nextui-org/react"
+import { useForm } from "react-hook-form"
+import { useRegisterMutation } from "../../app/services/userApi"
+import { hasErrorField } from "../../utils/has-error-field"
+import { ErrorMessage } from "../../components/error-message"
+
+type Register = {
+  email: string
+  name: string
+  password: string
+}
+
+type Props = {
+  setSelected: (value: string) => void
+}
+
+export const Register: React.FC<Props> = ({ setSelected }) => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<Register>({
+    mode: "onChange",
+    reValidateMode: "onBlur",
+    defaultValues: {
+      email: "",
+      password: "",
+      name: "",
+    },
+  })
+
+  const [register, { isLoading }] = useRegisterMutation()
+  const [error, setError] = useState("")
+
+  const onSubmit = async (data: Register) => {
+    try {
+      await register(data).unwrap()
+      setSelected("login")
+    } catch (error) {
+      if (hasErrorField(error)) {
+        setError(error.data.error)
+      }
+    }
+  }
+
+  return (
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        control={control}
+        name="name"
+        label="Name"
+        type="text"
+        required="Required fields"
+      />
+      <Input
+        control={control}
+        name="email"
+        label="Email"
+        // type="email"
+        required="Required fields"
+      />
+      <Input
+        control={control}
+        name="password"
+        label="Password"
+        type="password"
+        required="Required fields"
+      />
+
+      <ErrorMessage error={error} />
+
+      <p className="text-center text-small">
+        Already have an account?{" "}
+        <Link
+          size="sm"
+          className="cursor-pointer"
+          onPress={() => setSelected("login")}
+        >
+          Login
+        </Link>
+      </p>
+      <div className="flex gap-2 justify-end">
+        <Button fullWidth color="primary" type="submit" isLoading={isLoading}>
+          Register
+        </Button>
+      </div>
+    </form>
+  )
+}
