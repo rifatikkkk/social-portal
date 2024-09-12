064f17cf0be906624aa4fc8957df4cd8ec419172
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2024-09-12 14:23:55 +0300

    (client) follow user

--- client/src/pages/user-profile/index.tsx
+++ client/src/pages/user-profile/index.tsx
@@ -1,5 +1,113 @@
-import React from "react"
+import { Button, Card, Image, useDisclosure } from "@nextui-org/react"
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { useParams } from "react-router-dom"
+import { resetUser, selectCurrent } from "../../features/user/userSlice"
+import {
+  useFollowUserMutation,
+  useUnfollowUserMutation,
+} from "../../app/services/followApi"
+import {
+  useGetUserByIdQuery,
+  useLazyCurrentQuery,
+  useLazyGetUserByIdQuery,
+} from "../../app/services/userApi"
+import { GoBack } from "../../components/go-back"
+import { BASE_URL } from "../../constants"
+import {
+  MdOutlinePersonAddAlt1,
+  MdOutlinePersonAddDisabled,
+} from "react-icons/md"
+import { CiEdit } from "react-icons/ci"
+import { ProfileInfo } from "../../components/profile-info"
+import { formatToClientData } from "../../utils/format-to-client-data"
+import { CountInfo } from "../../components/count-info"
 
 export const UserProfile = () => {
-  return <div>UserProfile</div>
+  const { id } = useParams<{ id: string }>()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const currentUser = useSelector(selectCurrent)
+  const { data } = useGetUserByIdQuery(id ?? "")
+  const [followUser] = useFollowUserMutation()
+  const [unfollowUser] = useUnfollowUserMutation()
+  const [triggerGetUserByIdQuery] = useLazyGetUserByIdQuery()
+  const [triggerCurrentQuery] = useLazyCurrentQuery()
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(resetUser())
+  }, [])
+
+  if (!data) return null
+
+  const handleFollow = async () => {
+    try {
+      if (id) {
+        data?.isFollowing
+          ? await unfollowUser(id).unwrap()
+          : await followUser({ followingId: id }).unwrap()
+
+        await triggerGetUserByIdQuery(id)
+        await triggerCurrentQuery()
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <>
+      <GoBack />
+      <div className="flex items-center gap-4">
+        <Card className="flex flex-col items-center text-center space-y-4 p-5 flex-2">
+          <Image
+            src={`${BASE_URL}${data.avatarUrl}`}
+            alt={data.name}
+            width={200}
+            height={200}
+            className="border-4 border-white"
+          />
+
+          <div className="flex flex-col text-2xl font-bold gap-4 items-center">
+            {data.name}
+            {currentUser?.id !== id ? (
+              <Button
+                color={data.isFollowing ? "default" : "primary"}
+                variant="flat"
+                onClick={handleFollow}
+                className="gap-2"
+                endContent={
+                  data.isFollowing ? (
+                    <MdOutlinePersonAddDisabled />
+                  ) : (
+                    <MdOutlinePersonAddAlt1 />
+                  )
+                }
+              >
+                {data.isFollowing ? "Unsubscribe" : "Subscribe"}
+              </Button>
+            ) : (
+              <Button endContent={<CiEdit />}>Edit</Button>
+            )}
+          </div>
+        </Card>
+
+        <Card className="flex flex-col space-y-4 p-5 flex-1">
+          <ProfileInfo title="Email" info={data.email} />
+          <ProfileInfo title="Location" info={data.location} />
+          <ProfileInfo
+            title="Date of Birth"
+            info={formatToClientData(data.dateOfBirth)}
+          />
+          <ProfileInfo title="About me" info={data.bio} />
+
+          <div className="flex gap-2">
+            <CountInfo count={data.followers.length} title="Followers" />
+            <CountInfo count={data.following.length} title="Following" />
+          </div>
+        </Card>
+      </div>
+    </>
+  )
 }
