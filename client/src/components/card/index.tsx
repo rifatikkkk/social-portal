c10fb7e54ede2d9a0c28807d2059337cc24615e0
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2024-09-12 12:18:57 +0300

    (client) add and display post

---
+++ client/src/components/card/index.tsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react"
+
+import {
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Card as NextUiCard,
+  Spinner,
+} from "@nextui-org/react"
+import {
+  useLikePostMutation,
+  useUnlikePostMutation,
+} from "../../app/services/likesApi"
+import {
+  useDeletePostMutation,
+  useLazyGetAllPostsQuery,
+  useLazyGetPostByIdQuery,
+} from "../../app/services/postsApi"
+import { useDeleteCommentMutation } from "../../app/services/commentsApi"
+import { Link, useNavigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { selectCurrent } from "../../features/user/userSlice"
+import { User } from "../user"
+import { formatToClientData } from "../../utils/format-to-client-data"
+import { RiDeleteBinLine } from "react-icons/ri"
+import { Typography } from "../typography"
+import { MetaInfo } from "../meta-info"
+import { FcDislike } from "react-icons/fc"
+import { MdOutlineFavoriteBorder } from "react-icons/md"
+import { FaRegComment } from "react-icons/fa"
+import { ErrorMessage } from "../error-message"
+
+type Props = {
+  avatarUrl: string
+  name: string
+  authorId: string
+  content: string
+  commentId?: string
+  likesCount?: number
+  commentsCount?: number
+  createdAt?: Date
+  id?: string
+  cardFor: "comment" | "post" | "current-post"
+  likedByUser?: boolean
+}
+
+export const Card: React.FC<Props> = ({
+  avatarUrl = "",
+  name = "",
+  authorId = "",
+  content = "",
+  commentId = "",
+  likesCount = 0,
+  commentsCount = 0,
+  createdAt,
+  id = "",
+  cardFor = "post",
+  likedByUser = false,
+}) => {
+  const [likePost] = useLikePostMutation()
+  const [unlikePost] = useUnlikePostMutation()
+  const [triggerGetAllPosts] = useLazyGetAllPostsQuery()
+  const [triggerGetPostById] = useLazyGetPostByIdQuery()
+  const [deletePost, deletePostStatus] = useDeletePostMutation()
+  const [deleteComment, deleteCommentStatus] = useDeleteCommentMutation()
+  const [error, serError] = useState("")
+  const navigate = useNavigate()
+  const currentUser = useSelector(selectCurrent)
+
+  return (
+    <NextUiCard className="mb-5">
+      <CardHeader className="justify-between items-center bg-transparent">
+        <Link to={`/users/${authorId}`}>
+          <User
+            name={name}
+            className="text-small font-semibold leading-none text-default-600"
+            avatarUrl={avatarUrl}
+            description={createdAt && formatToClientData(createdAt)}
+          />
+        </Link>
+        {authorId === currentUser?.id && (
+          <div className="cursor-pointer">
+            {deletePostStatus.isLoading || deleteCommentStatus.isLoading ? (
+              <Spinner />
+            ) : (
+              <RiDeleteBinLine />
+            )}
+          </div>
+        )}
+      </CardHeader>
+      <CardBody className="px-3 py-2 mb-5">
+        <Typography>{content}</Typography>
+      </CardBody>
+      {cardFor !== "comment" && (
+        <CardFooter className="gap-3">
+          <div className="flex gap-5 items-center">
+            <div>
+              <MetaInfo
+                count={likesCount}
+                Icon={likedByUser ? FcDislike : MdOutlineFavoriteBorder}
+              />
+            </div>
+
+            <Link to={`/posts/${id}`}>
+              <MetaInfo count={commentsCount} Icon={FaRegComment} />
+            </Link>
+          </div>
+
+          <ErrorMessage error={error} />
+        </CardFooter>
+      )}
+    </NextUiCard>
+  )
+}
---
+++ client/src/components/create-post/index.tsx
@@ -0,0 +1,62 @@
+import React from "react"
+import {
+  useCreatePostMutation,
+  useLazyGetAllPostsQuery,
+} from "../../app/services/postsApi"
+import { Controller, useForm } from "react-hook-form"
+import { Button, Textarea } from "@nextui-org/react"
+import { ErrorMessage } from "../error-message"
+import { IoMdCreate } from "react-icons/io"
+
+export const CreatePost = () => {
+  const [createPost] = useCreatePostMutation()
+  const [triggerAllPosts] = useLazyGetAllPostsQuery()
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    setValue,
+  } = useForm()
+
+  const error = errors?.post?.message as string
+
+  const onSubmit = handleSubmit(async data => {
+    try {
+      await createPost({ content: data.post }).unwrap()
+      setValue("post", "")
+      await triggerAllPosts().unwrap()
+    } catch (error) {
+      console.log(error)
+    }
+  })
+  return (
+    <form className="flex-grow" onSubmit={onSubmit}>
+      <Controller
+        name="post"
+        control={control}
+        defaultValue=""
+        rules={{ required: "Required fields" }}
+        render={({ field }) => (
+          <Textarea
+            {...field}
+            labelPlacement="outside"
+            placeholder="Your thoughts"
+            className="mb-5"
+          />
+        )}
+      />
+
+      {errors && <ErrorMessage error={error} />}
+
+      <Button
+        color="success"
+        className="flex-end"
+        endContent={<IoMdCreate />}
+        type="submit"
+      >
+        Add a post
+      </Button>
+    </form>
+  )
+}
